050fa392076e32d8ae04d8148b35eac3d100454c
ajoygp <41632101+ajoygp@users.noreply.github.com>
2022-11-25 12:48:02 +0530

    cards are now clicabkle

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,20 @@ import { useState } from 'react'
 import './App.css'
 import Card from './components/Card'
 
+
 function App() {
   
-  const cards= [1,2,3,4,5,6,7,8,9,10,11,12].map(i=><Card value={i}/>)
+  const [cards,setCards] = useState([1,2,3,4,5,6,7,8,9,10,11,12].map(i=>({value:i,isClicked:false})))
+  
+  function markCardClicked(value){
+    setCards((oldCards)=>oldCards.map(card=>{
+      return {...card, 
+        isClicked:card.value===value?true:card.isClicked}
+      }))
+    }
+    
+  const cardElements = cards.map(card=><Card key={card.value} handleClick={markCardClicked} value={card.value} isClicked={card.isClicked}/>)
+
   return (
     <div className="App">
       <header>
@@ -14,8 +25,9 @@ function App() {
           <h2 className='score'>Top Score:0</h2>
         </div>
       </header>
+
       <div className='card-holder'>
-        {cards}
+        {cardElements}
       </div>
       
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Card(props) {
   return (
-    <div className='card'>
+    <div className='card' onClick={()=>props.handleClick(props.value)}>
         <h2 >{props.value}</h2>
     </div>
   )
